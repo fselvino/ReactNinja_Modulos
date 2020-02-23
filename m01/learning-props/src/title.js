3fa3efe2fcd8ddcb61179f57eebb9f3947c627ff
unknown <fernandoselvino@gmail.com>
2020-02-22 22:06:11 -0300

    teste

--- m01/learning-props/src/title.js
+++ m01/learning-props/src/title.js
@@ -2,21 +2,33 @@
 
 import React from 'react'
 
-const Title = React.createClass({
+const Title = (props) => {
+  // utilizando templeit strings
+  return (
+    <h1>Olá {`${props.name} ${props.lastname}`}</h1>
+  )
+}
+Title.defaultProps = {
+  name: 'Desconhecido',
+  lastname: 'Sem sobrenome'
+}
+// const Title = React.createClass({
 
-  // getDefaultProps podemos definir as propriedades padrão
-  getDefaultProps: function () {
-    return {
-      name: 'Desconhecido',
-      lastname: 'Sem sobrenome'
-    }
-  },
-  render: function () {
-    return (
-      <h1>Ola {this.props.name + ' ' + this.props.lastname} </h1>
+//   }
 
-    )
-  }
-})
+//   // // getDefaultProps podemos definir as propriedades padrão
+//   // getDefaultProps: function () {
+//   //   return {
+//   //     name: 'Desconhecido',
+//   //     lastname: 'Sem sobrenome'
+//   //   }
+//   // },
+//   // render: function () {
+//   //   return (
+//   //     <h1>Ola {this.props.name + ' ' + this.props.lastname} </h1>
+
+//   //   )
+//   // }
+// })
 
 export default Title
