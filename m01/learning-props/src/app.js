c437dd7c7253b4276e7bbe5a39fbaf8089002853
unknown <fernandoselvino@gmail.com>
2020-02-23 10:27:57 -0300

    Modulo 1 Aula 16 - props children

--- m01/learning-props/src/app.js
+++ m01/learning-props/src/app.js
@@ -2,21 +2,19 @@
 
 // var React = require('react')
 import React, { Component } from 'react'
+import Button from './button'
 // import Title from './title'
-import Square from './square'
+// import Square from './square'
 
 class App extends Component {
   render() {
-    // estudo de eventos
-    // envento onClick ocorre quando o usuario click na tela
+    // estudo de children
+    // e possivel pegar as informaçoes que estiver dentro das tags do 
+    // componente
 
     return (
-      <div
-        className='container'
-        onClick={(e) => {
-          alert('clicou')
-        }}>
-        <Square />
+      <div>
+        <Button>Botão2</Button>
       </div>
     )
   }
